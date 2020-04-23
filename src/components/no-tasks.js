c7f6eb1d25c5580c7e294760af65429ab6f211d0
Gleb Dolgushin <dolgushing@gmail.com>
2020-04-23 07:07:55 +0300

    Правки

--- src/components/no-tasks.js
+++ src/components/no-tasks.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractComponent from "./abstract-component.js";
 
 const createNotasksTemplate = () => {
   return (
@@ -8,21 +8,8 @@ const createNotasksTemplate = () => {
   );
 };
 
-export default class NoTasks {
-  constructor() {
-    this._element = null;
-  }
-
+export default class NoTasks extends AbstractComponent {
   getTemplate() {
     return createNotasksTemplate();
-  }
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-  removeElement() {
-    this._element = null;
   }
 }
